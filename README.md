27bf4934c0d9a485b89c8495a79adb94336aab68
Leonardo <leopiccionia@gmail.com>
2018-09-30 17:32:27 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,40 @@
+# nuxt-feathers-boilerplate
+
+A full-stack boilerplate leveraging Nuxt 2.x and Feathers 3.x.
+
+It's still **work-in-progress**. Use at your own risk.
+
+## Features
+
+* Hot-reloading on back-end and front-end
+* REST and SocketIO pre-installed
+
+### Roadmap
+
+* Linting
+* Testing
+* Integration with `feathers-vuex` (once it supports Feathers 3.x and Vuex 3.x)
+
+## Useful commands
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# run @featherjs/cli commands
+$ yarn server generate service      # generate new Feathers service
+$ yarn server generate hook         # generate new Feathers hook
+$ yarn server help
+
+# generate static project
+$ yarn static
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js](https://nuxtjs.org) and [Feathers](https://feathersjs.com/) docs.
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="container">
+    <div>
+      <img src="~/assets/logo.svg">
+      <h1 class="title">nuxt-feathers-boilerplate</h1>
+      <h2 class="subtitle">A full-stack boilerplate leveraging Nuxt 2.x and Feathers 3.x</h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Nuxt docs</a>
+        <a href="https://feathersjs.com" target="_blank" class="button--grey">Feathers docs</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 60px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 30px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
