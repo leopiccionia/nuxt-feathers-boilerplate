97eeab65bd6cadec196ed83c33754732bf09abbd
Leonardo <leopiccionia@gmail.com>
2018-09-30 21:09:11 -0300

    Lint: always use semicolons.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
     'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
